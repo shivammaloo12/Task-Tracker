6155bd4e9c20d6e95ac3514da71a9a88ab6473cb
Shivam Maloo <shivammaloo12@gmail.com>
2025-10-02 19:25:57 +0530

    Task Tracker

---
+++ src/main/java/com/Shivam/Model/Task.java
@@ -0,0 +1,11 @@
+package com.Shivam.Model;
+
+public class Task {
+    private int id;
+    private String description;
+    private String status;
+    private String createdAt;
+    private String updatedAt;
+
+
+}
